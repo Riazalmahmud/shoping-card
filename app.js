dcdbd353129acab031b6243615c826d484155164
Riazalmahmud <riazalmahmud041@gmail.com>
2021-08-17 22:20:03 +0600

    phone update

--- app.js
+++ app.js
@@ -8,18 +8,42 @@
 
 
 // phone event Increase decrage 
+function getPhoneUpdate(isIncreasePhone) {
+
+    const phoneField = document.getElementById('phone-field');
+    let phoneText = phoneField.value;
+
+    if (isIncreasePhone == true) {
+        phoneText = parseInt(phoneText) + 1;
+    }
+    else if (phoneText > 0) {
+        phoneText = parseInt(phoneText) - 1;
+    }
+    phoneField.value = phoneText;
+
+
+
+    // update phone  price 
+
+    const phonePrice = document.getElementById('phone-price');
+    phonePrice.innerText = phoneText * 1219;
+}
+
+
 document.getElementById('phone-plus').addEventListener('click', function () {
-
+    getPhoneUpdate(true)
 
 });
+
 document.getElementById('phone-minus').addEventListener('click', function () {
-
+    getPhoneUpdate(false)
 });
 
-// handale event incrace decrace 
+//casing handale event incrace decrace 
 
 
 function getCasingUpdate(isIncrease) {
+
     const casingNumber = document.getElementById('casing-number');
     let casingNumberText = casingNumber.value;
 
@@ -33,6 +57,8 @@ function getCasingUpdate(isIncrease) {
     // update casing 
     const casingPrice = document.getElementById('casing-price');
     casingPrice.innerText = casingNumberText * 59;
+
+
 
 }
 document.getElementById('casing-plus').addEventListener('click', function () {
